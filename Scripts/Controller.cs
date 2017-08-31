cf97eab9065e121e2cf8ff3f8a632bdbbbf121fb
Billy O'Mahony <omahonybilly@gmail.com>
2017-08-31 22:35:00 +0100

    Basic Camera Controller
    Also rearranged files

---
+++ Scripts/Controller.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour {
+
+    public float AccelerationTime = 1f;
+    [Range(0f, 1f)]
+    public float CurrentSpeedMultiplier = 0f;
+
+    void Update()
+    {
+        if (Input.GetButton("Horizontal"))
+        {
+            transform.Translate(Input.GetAxis("Horizontal"), 0, 0, Space.World);
+        }
+
+        if (Input.GetButton("Vertical"))
+        {
+            transform.Translate(0, 0, Input.GetAxis("Vertical"), Space.World);
+        }
+
+    }
+}
