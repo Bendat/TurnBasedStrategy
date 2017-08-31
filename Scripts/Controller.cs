e3705f5a3503e26a622cfc841abaa5b0e14a8ab2
Billy O'Mahony <omahonybilly@gmail.com>
2017-08-31 23:18:34 +0100

    Camera Movement
    Camera accelerates to max speed

--- Scripts/Controller.cs
+++ Scripts/Controller.cs
@@ -4,21 +4,87 @@ using UnityEngine;
 
 public class Controller : MonoBehaviour {
 
-    public float AccelerationTime = 1f;
+    public float AccelerationTime, ScrollSpeed, timer;
+
     [Range(0f, 1f)]
-    public float CurrentSpeedMultiplier = 0f;
+    public float CurrentSpeedMultiplier;
+
+    public bool HorizontalPressed, VerticalPressed;
+
+    void Start()
+    {
+        CurrentSpeedMultiplier = 0f;
+        HorizontalPressed = false;
+        VerticalPressed = false;
+        timer = 0f;
+    }
 
     void Update()
     {
+        CheckMovementInputs();
+    }
+
+    /// <summary>
+    /// Deals with horizontal and forward movement of camera
+    /// </summary>
+    void CheckMovementInputs()
+    {
         if (Input.GetButton("Horizontal"))
         {
-            transform.Translate(Input.GetAxis("Horizontal"), 0, 0, Space.World);
+            HorizontalPressed = true;
+            transform.Translate(CurrentSpeedMultiplier * Input.GetAxis("Horizontal") * ScrollSpeed * Time.deltaTime, 0, 0, Space.World);
+            if (CurrentSpeedMultiplier == 0f)
+            {
+                StartCoroutine(ScrollAcceleration());
+            }
+        }
+
+        if (Input.GetButtonUp("Horizontal"))
+        {
+            HorizontalPressed = false;
+            ResetSpeedMultiplier();
         }
 
         if (Input.GetButton("Vertical"))
         {
-            transform.Translate(0, 0, Input.GetAxis("Vertical"), Space.World);
+            VerticalPressed = true;
+            transform.Translate(0, 0, CurrentSpeedMultiplier * Input.GetAxis("Vertical") * ScrollSpeed * Time.deltaTime, Space.World);
+            if (CurrentSpeedMultiplier == 0f)
+            {
+                StartCoroutine(ScrollAcceleration());
+            }
         }
 
+        if (Input.GetButtonUp("Vertical"))
+        {
+            VerticalPressed = false;
+            ResetSpeedMultiplier();
+        }
+    }
+
+    /// <summary>
+    /// If neither scroll button is pressed speed multiplier reset to 0
+    /// </summary>
+    void ResetSpeedMultiplier()
+    {
+        if (!VerticalPressed && !HorizontalPressed)
+        {
+            CurrentSpeedMultiplier = 0f;
+        }
+    }
+
+    /// <summary>
+    /// Accelerates the camera to max speed over AccelerationTime
+    /// </summary>
+    /// <returns></returns>
+    IEnumerator ScrollAcceleration()
+    {
+        timer = 0f;
+        while(timer < AccelerationTime && (HorizontalPressed || VerticalPressed))
+        {
+            timer += Time.deltaTime;
+            CurrentSpeedMultiplier = Mathf.Lerp(0, 1, timer / AccelerationTime);
+            yield return null;
+        }
     }
 }
