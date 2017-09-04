41b126df59ac436ceb460922cea7e08ea77a0487
Ben Aherne <Bendat@users.noreply.github.com>
2017-09-04 16:16:46 +0100

    Updated camera script to use manager, added documentation

--- Scripts/Utils/AbstraceSingleton.cs
+++ Scripts/Utils/AbstraceSingleton.cs
@@ -1,10 +1,15 @@
 ﻿namespace Assets.TurnBasedStrategy.Scripts.Utils
 {
-    public class AbstraceSingleton<T>
+    /// <summary>
+    /// Abstract base for for Singleton type classes. Only allows one instance of each class to exist.
+    /// </summary>
+    /// <typeparam name="T">Generic Type which inherits AbstractSingleton and is of type object.</typeparam>
+    public abstract class AbstraceSingleton<T>
         where T : AbstraceSingleton<T>, new()
     {
-        private static T _instance = new T();
-        public static T Inst => _instance;
-
+        /// <summary>
+        /// The living instance object of the class T.
+        /// </summary>
+        public static T Inst { get; } = new T();
     }
 }
