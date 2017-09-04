9c3eae30020d812fa08d0fe58bfc60017a5a2518
Billy O'Mahony <omahonybilly@gmail.com>
2017-09-04 19:28:13 +0100

    Adding TurnManager.cs

---
+++ Scripts/TurnSystem/TurnManager.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TurnManager : MonoBehaviour {
+
+    TurnManagerUI _tmUI;
+
+    /// <summary>
+    /// The Current Turn Number
+    /// </summary>
+    public int TurnNumber = 0;
+
+    /// <summary>
+    /// A list of all players in the game
+    /// </summary>
+    public List<Player> Players;
+
+    /// <summary>
+    /// The list index of the current player
+    /// </summary>
+    public int CurrentPlayer = 0;
+
+    void Start()
+    {
+        _tmUI = GetComponent<TurnManagerUI>();
+
+        TurnStart();
+    }
+
+    /// <summary>
+    /// Called on the start of a players turn
+    /// </summary>
+    public void TurnStart()
+    {
+        // Iterate the turn if it's back to the first player
+        if(CurrentPlayer == 0)
+        {
+            TurnNumber ++;
+        }
+
+        Players[CurrentPlayer].SetPlayersTurn(true);
+
+        _tmUI.UpdateTurnNumber(TurnNumber);
+        _tmUI.UpdateCurrentPlayerText(Players[CurrentPlayer].PlayerName);
+    }
+
+    /// <summary>
+    /// Ends the current players turn
+    /// </summary>
+    public void EndTurn()
+    {
+        Players[CurrentPlayer].SetPlayersTurn(false);
+
+        CurrentPlayer++;
+        if (CurrentPlayer >= Players.Count) CurrentPlayer = 0;
+        TurnStart();
+    }
+    
+}
---
+++ Scripts/UI/TurnManager/TurnManagerUI.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TurnManagerUI : MonoBehaviour {
+
+    public Text TurnNumberText;
+    public Text CurrentPlayerText;
+
+    public void UpdateTurnNumber(int turn)
+    {
+        TurnNumberText.text = "Turn: " + turn;
+    }
+
+    public void UpdateCurrentPlayerText(string PlayerName)
+    {
+        CurrentPlayerText.text = PlayerName;
+    }
+
+}
