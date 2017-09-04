8beed3007ee7cde4689d7ffa4f41dc1340cef05a
Ben Aherne <Bendat@users.noreply.github.com>
2017-09-04 16:06:21 +0100

    added AbstractInputManager and CameraInputManager

---
+++ Scripts/Common/Input Managers/CameraInputManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Assets.TurnBasedStrategy.Scripts.Utils;
+using UnityEngine;
+
+namespace Assets.TurnBasedStrategy.Scripts.Common.Input_Managers
+{
+    public class CameraInputManager: AbstraceSingleton<CameraInputManager>
+    {
+        private float Horizontal => Input.GetAxis("Horizontal");
+        private float Vertical => Input.GetAxis("Vertical");
+        private float Scroll => Input.GetAxis("Mouse ScrollWheel");
+        private float Rotation => Input.GetAxis("Rotation");
+    }
+}
---
+++ Scripts/Utils/AbstraceSingleton.cs
@@ -0,0 +1,10 @@
+﻿namespace Assets.TurnBasedStrategy.Scripts.Utils
+{
+    public class AbstraceSingleton<T>
+        where T : AbstraceSingleton<T>, new()
+    {
+        private static T _instance = new T();
+        public static T Inst => _instance;
+
+    }
+}
