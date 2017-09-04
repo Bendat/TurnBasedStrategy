e5ada597b7e21f1a0ca16883dc415058fadbdf09
Ben Aherne <Bendat@users.noreply.github.com>
2017-09-04 17:21:46 +0100

    Revert "Revert "Added Player.cs and TurnManager.cs""
    This reverts commit 15c581b82f83e2379499ca3d7af5c71251eb6118.

---
+++ Scripts/TurnSystem/Player.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// The main class for each player
+/// </summary>
+public class Player : MonoBehaviour {
+
+    public string PlayerName;
+
+    /// <summary>
+    /// If true it is this players turn
+    /// </summary>
+    bool _isTurn;
+
+    /// <summary>
+    /// If true this player is an AI player
+    /// </summary>
+    bool _isAI;
+
+    public List<string> Buildings;
+    public List<string> Units;
+
+    /// <summary>
+    /// Used to activate or deactivate this player
+    /// </summary>
+    /// <param name="isTurn"></param>
+    public void SetPlayersTurn(bool isTurn)
+    {
+        _isTurn = isTurn;
+    }
+
+    /// <summary>
+    /// calls the refresh abstract method of all buildings and units
+    /// </summary>
+    void Refresh()
+    {
+
+    }
+
+}
