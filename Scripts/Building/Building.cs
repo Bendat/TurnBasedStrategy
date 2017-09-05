5a1f75661fa8317257dd4ce1c5f14ca7d0443d7b
Billy O'Mahony <omahonybilly@gmail.com>
2017-09-05 15:32:05 +0100

    Created Unit and Building abstract classes

---
+++ Scripts/Building/Building.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Building : MonoBehaviour {
+
+    public string BuildingName;
+
+    public abstract void Refresh();
+}
